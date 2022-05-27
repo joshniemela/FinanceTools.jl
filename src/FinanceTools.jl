56a2f47153823bc12be47c41b01570cd0b04814e
Josh Niemela <josh@jniemela.dk>
2022-05-27 20:35:04 +0200

    Added a fractional differentation operator

--- src/FinanceTools.jl
+++ src/FinanceTools.jl
@@ -1,5 +1,10 @@
 module FinanceTools
 import DataFrames: DataFrame
+import FFTW: fft, ifft
+
+export split_adjust
+export fracdiff
+
 function split_adjust(df::DataFrame)
     close = copy(df.close)
     shifted = circshift(close, 1)
@@ -13,5 +18,19 @@ function split_adjust(df::DataFrame)
 
     return df
 end
-export split_adjust
+
+function fracdiff(x::AbstractVector, d::Real)
+    T = length(x)
+    np₂ = Int(2^ceil(log2(2T-1)))
+    k = 1:(T-1)
+    b = cumprod((k.-(d).-1)./k)
+    pushfirst!(b, 1)
+    z = zeros(np₂-T)
+    pushfirst!(z, 0)
+    z₁ = vcat(b, z)
+    z₂ = vcat(x, z)
+    dx = ifft(fft(z₁).*fft(z₂))
+    return real(getindex(dx, 1:T))
+end
+
 end # module
