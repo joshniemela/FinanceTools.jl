8b200a5a0cfa5b548743ea7391c25773483695fe
Josh Niemela <josh@jniemela.dk>
2022-05-27 17:57:24 +0200

    Added split adjust

--- src/FinTools.jl
+++ src/FinTools.jl
@@ -1,5 +1,17 @@
-module InfoDrivenBars
+module FinTools
 
-greet() = print("Hello World!")
+function split_adjust(df)
+    close = df.close
+    shifted = circshift(close, 1)
+    cumratio = cumprod(div.(shifted, close, RoundNearest))
+
+    df.close = df.close .* cumratio
+    df.high = df.high .* cumratio
+    df.low = df.low .* cumratio
+    df.open = df.open .* cumratio
+    df.volume = df.volume ./ cumratio
+
+    return df
+end
 
 end # module
