081d4d3e01f7e79c4d043f31fc81480d8b4c980a
Josh Niemela <josh@jniemela.dk>
2022-06-02 18:38:22 +0200

    TagBot and typo fix

--- src/labeling.jl
+++ src/labeling.jl
@@ -6,6 +6,7 @@ link : https://pdfs.semanticscholar.org/9ab2/13b22d49099256b1c98e476be2022922e8f
 
 Code will be optimised later on
 """
+
 function trendlabel(x::AbstractArray, ω::Real)
     N = length(x) # paper uses N as length of vector
     t = collect(1:N) # paper uses this thing for some reason
@@ -29,6 +30,37 @@ function trendlabel(x::AbstractArray, ω::Real)
             break
         end
     end
-    return y
+
+    for i in FP_N+1:N
+        # Trending down
+        if Cid > 0
+            if x[i] > xₕ
+                xₕ, HT = x[i], t[i]
+            end
+            if x[i] < xₕ - xₕ * ω && LT ≤ HT
+                for j in 1:N
+                    if t[j] > LT && t[j] ≤ HT
+                        y[j] = 1
+                    end
+                end
+                xₗ, LT, Cid = x[i], t[i], -1
+            end
+        end
+        # Trending up
+        if Cid < 0
+            if x[i] < xₗ
+                xₗ, LT = x[i], t[i]
+            end
+            if x[i] > xₗ + xₗ * ω && HT ≤ LT
+                for j in 1:N
+                    if t[j] > HT && t[j] ≤ LT
+                        y[j] = -1
+                    end
+                end
+                xₕ, HT, Cid = x[i], t[i], 1
+            end
+        end
+    end
+    return (y)
 end
 export trendlabel
