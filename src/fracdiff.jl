af640a2aeba53542f1511d40021b5d554118f918
Joshua Niemelä <josh@jniemela.dk>
2022-06-12 01:09:27 +0200

    Update fracdiff.jl

--- src/fracdiff.jl
+++ src/fracdiff.jl
@@ -12,11 +12,25 @@ function weights(d::Number, cutoff::Number)
     end
     return (ω)
 end
-
+    
+"""
+Calculates the fractional difference of a vector of numbers.
+# Keyword arguments
+- `d` : Order of difference, applies the n'th order of fractional difference to the series. Integer orders result in the same as the lag operator.
+- `cutoff` : The minimum value of a term in the binomial weights, lower values will result in a more precise result with a higher computatinal cost.
+"""
 function fracdiff(data::AbstractArray, d::Number, cutoff::Number=1e-3)
     conv(data, weights(d, cutoff))[1:length(data)]
 end
 
+"""
+Inplace version of the fracdiff function.
+    
+Calculates the fractional difference of a vector of numbers.
+# Keyword arguments
+- `d` : Order of difference, applies the n'th order of fractional difference to the series. Integer orders result in the same as the lag operator.
+- `cutoff` : The minimum value of a term in the binomial weights, lower values will result in a more precise result with a higher computatinal cost.
+"""    
 function fracdiff!(data::AbstractArray, d::Number, cutoff::Number=1e-3)
     data .= conv(data, weights(d, cutoff))[1:length(data)]
 end
