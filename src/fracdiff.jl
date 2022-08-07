4ace309bb22a1772c2f11f4a931c2ee8f42dfaeb
Joshua Niemelä <josh@jniemela.dk>
2022-08-07 17:30:25 +0200

    Typo

--- src/fracdiff.jl
+++ src/fracdiff.jl
@@ -17,7 +17,7 @@ end
 Calculates the fractional difference of a vector of numbers.
 # Arguments
 - `d::Real` : Order of difference, applies the n'th order of fractional difference to the series. Integer orders result in the same as the lag operator.
-- `cutoff::Real=1e-3` : The minimum value of a term in the binomial weights, lower values will result in a more precise result with a higher computatinal cost.
+- `cutoff::Real=1e-3` : The minimum value of a term in the binomial weights, lower values will result in a more precise result with a higher computational cost.
 
 #### Example
 ```julia
